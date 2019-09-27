474ef4f5089d58a786244fe9084df26fce0e6c38
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-09-27 13:20:06 -0600

    Face Detection Box

--- src/components/FaceRecorgnition/FaceRecorgnition.js
+++ src/components/FaceRecorgnition/FaceRecorgnition.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './FaceRecorgnition.css';
 
 
 
-const FaceRecorgnition = ({ imageUrl }) => {
+const FaceRecorgnition = ({ imageUrl, box }) => {
     return (
         <div className='center ma'>
             <div className='absolute mt2'>
-                <img alt='' src={imageUrl} width='500px' height='auto'/>
+                <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto'/>
+                <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
             </div>
         </div>
     );
